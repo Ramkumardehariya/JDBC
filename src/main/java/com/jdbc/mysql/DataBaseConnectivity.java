722d29a192fa7dc0dc1537bfbab4a725293cfa2b
Ramkumar Dehariya <ramkumardehariya26@gmail.com>
2024-10-28 15:23:01 +0530

    JDBC using mys1ql

---
+++ src/main/java/com/jdbc/mysql/DataBaseConnectivity.java
@@ -0,0 +1,15 @@
+package com.jdbc.mysql;
+
+import java.sql.SQLException;
+
+public class DataBaseConnectivity {
+	 public static void main(String[] args) throws SQLException {
+	        JDBCExample jdbcExample = new JDBCExample();
+//	        System.out.println("Connection Successful"+jdbcExample.getConnection());
+//	        jdbcExample.createTable();
+//	        jdbcExample.createEmployee(new Employee(1,"Sample1",25));
+//	        jdbcExample.createEmployee(new Employee(2,"Sample2",30));
+//	        jdbcExample.createEmployee(new Employee(3,"Sample3",18));
+	         jdbcExample.readEmployee();
+	    }
+}
